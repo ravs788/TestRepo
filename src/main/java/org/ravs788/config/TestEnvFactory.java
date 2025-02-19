3b138a57a731dbbcafd6d5c40ed3bf94067f0da0
Ravi Shankar <ravs788@gmail.com>
2025-02-20 05:01:29 +0530

    Testing reading configs for different environments using Typesafe

---
+++ src/main/java/org/ravs788/config/TestEnvFactory.java
@@ -0,0 +1,43 @@
+package org.ravs788.config;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.Objects;
+
+@Slf4j
+public class TestEnvFactory {
+    private static final TestEnvFactory UNIQUE_INSTANCE = new TestEnvFactory();
+    private Config config;
+
+    private TestEnvFactory() {
+        config = setConfig();
+    }
+
+    public Config getConfig(){
+        return config;
+    }
+
+    public static TestEnvFactory getInstance(){
+        return UNIQUE_INSTANCE;
+    }
+
+    private Config setConfig(){
+        log.info("setConfig called");
+        // As per https://github.com/lightbend/config?tab=readme-ov-file#standard-behavior
+        config = ConfigFactory.load();
+
+        TestEnv testEnv = config.getEnum(TestEnv.class, "TEST_ENV");
+        String testEnvDirPath = String.format("src/main/resources/%s", testEnv);
+        File testEnvDir = new File(testEnvDirPath);
+        for (File file: Objects.requireNonNull(testEnvDir.listFiles())){
+            Config childConf = ConfigFactory.load(String.format("%s/%s", testEnv, file.getName()));
+            config = config.withFallback(childConf);
+        }
+
+        return config;
+    }
+
+}
