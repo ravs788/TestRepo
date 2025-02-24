a60e1505b90a0237234effe3edc3635342d2b850
Ravi Shankar <ravs788@gmail.com>
2025-02-24 06:53:59 +0530

    added a new workflow file and additional test files

---
+++ src/test/java/TestServiceA.java
@@ -0,0 +1,31 @@
+import com.typesafe.config.Config;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.ravs788.config.TestEnvFactory;
+import org.ravs788.config.annotations.FailingTest;
+import org.ravs788.config.annotations.FlakyTest;
+import org.ravs788.config.annotations.SmokeTest;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("service-A")
+@Slf4j
+public class TestServiceA {
+
+    @SmokeTest
+    void assertThatTrueIsTrue(){
+        assertTrue(true, "true is true");
+    }
+
+    @SmokeTest
+    void assertThatTestForChosenEnvRuns(){
+        Config CONFIG = TestEnvFactory.getInstance().getConfig();
+        String expectedEnv = CONFIG.getString("TEST_ENV");
+        log.info("expectedEnv is {}", expectedEnv);
+        assertEquals(expectedEnv, CONFIG.getString("TEST_ENV"), "TEST_ENV");
+    }
+
+}
